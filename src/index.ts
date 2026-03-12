ee5bd9c4ae1c7b5fd5a08c365c4c1aa599e893f3
Drew Neil <andrew.jr.neil@gmail.com>
2026-03-12 20:03:11 +0000

    Wire up public API in index.ts and fix TypeScript build

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 export const version = '1.0.0';
+
+// Parser
+export { parseSong, parseChord, parseBar, parseRow, parseFrontMatter } from './parser/parser.js';
+export type { Song, Row, Bar, Chord, Quality, FrontMatter } from './parser/parser.js';
+
+// Renderer
+export { TextRenderer } from './renderers/text.js';
+
+// Normaliser
+export { normaliseSong } from './theory/normalise.js';
+export type { DetectKeyConfig } from './theory/keyDetector.js';
+
+// Theory utilities
+export { detectKey } from './theory/keyDetector.js';
+export { diatonicNotes } from './theory/keys.js';
+export { rootToPitchClass, ENHARMONIC_PAIRS } from './theory/pitchClass.js';
