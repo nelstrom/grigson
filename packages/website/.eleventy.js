bc7fe8bc4be055ab5346df9895e39ffd8e13f955
Drew Neil <andrew.jr.neil@gmail.com>
2026-03-13 11:03:16 +0000

    feat(website): enable TextMate-based highlighting in Monaco playground
    - Install monaco-textmate and vscode-oniguruma in packages/website
    - Update website/.eleventy.js to passthrough onig.wasm
    - Integrate monaco-textmate in playground.njk to provide syntax highlighting
    - Define 'grigson-theme' in Monaco mapping TM scopes to colors

--- packages/website/.eleventy.js
+++ packages/website/.eleventy.js
@@ -4,6 +4,7 @@ export default function (eleventyConfig) {
     'node_modules/grigson/dist/grigson.iife.js': 'js/grigson.iife.js',
     '../textmate-grammar/grigson.tmLanguage.json': 'js/grigson.tmLanguage.json',
     'node_modules/monaco-editor/min/vs': 'js/monaco/vs',
+    'node_modules/vscode-oniguruma/release/onig.wasm': 'js/onig.wasm',
   });
 
   return {
