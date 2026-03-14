37777f5a18998adb149111b0a8a3da4c410a4e06
Drew Neil <andrew.jr.neil@gmail.com>
2026-03-14 23:40:37 +0000

    feat(tree-sitter): implement chord grammar with note_letter, accidental, quality rules

--- packages/tree-sitter-grammar/grammar.js
+++ packages/tree-sitter-grammar/grammar.js
@@ -1,9 +1,21 @@
 module.exports = grammar({
   name: 'grigson',
 
-  extras: ($) => [/[ \t]/],
+  extras: (_$) => [/[ \t\r\n]/],
 
   rules: {
-    song: ($) => repeat(/[^\n]*\n/),
+    song: ($) => repeat($.chord),
+
+    chord: ($) =>
+      seq($.note_letter, optional($.accidental), optional($.quality)),
+
+    note_letter: (_$) => /[A-G]/,
+
+    accidental: (_$) => token.immediate(/[#b]/),
+
+    quality: (_$) =>
+      token.immediate(
+        choice('m7b5', 'maj7', 'M7', 'dim7', 'm7', 'dim', 'm', '7'),
+      ),
   },
 });
